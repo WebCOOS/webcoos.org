8efa2858132f8ca1e21f7ec53c4288cf112bfb51
Kyle Wilcox <kyle@axiomdatascience.com>
2022-03-22 09:28:53 -0400

    Adding the Camera components from landing-page-components 0.4.0

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,31 @@ import {
 import Page from '../components/Page';
 import { getSiteMetadata, getYaml } from '../utils';
 
+import dynamic from 'next/dynamic';
+
+const CameraLandingSection = dynamic(
+    () => (
+        import('@axds/landing-page-components/build/es/no-ssr')
+            .then(mod => mod.CameraLandingSection)
+    ), { ssr: false })
+
+
 export default function Home({ content, metadata }) {
     return (
         <Page metadata={metadata}>
             <HeroSection {...content.sections.hero} />
 
             <Section shaded={true}>
+                <SectionHeader>Cameras</SectionHeader>
+                <CameraLandingSection
+                    apiUrl="https://app.stage.webcoos.org/webcoos/api"
+                    apiVersion="v1"
+                    token="219ec2bd77f37ce8bbb12e4c08a7b62bd506a7d3"
+                    source="webcoos"
+                />
+            </Section>
+
+            <Section>
                 <SectionHeader>Project Partners</SectionHeader>
                 <PartnerLogos
                     partners={content.sections.partners}
@@ -22,7 +41,7 @@ export default function Home({ content, metadata }) {
                 />
             </Section>
 
-            <NarrativeSection {...content.sections.funding} />
+            <NarrativeSection shaded={true} {...content.sections.funding} />
         </Page>
     );
 }
