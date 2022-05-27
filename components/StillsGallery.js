e99d1feb3a711ec7fe7b5afc663c98e329bff2d8
Dave Foster <dave@axiomdatascience.com>
2022-05-26 23:31:20 -0400

    Simple pagination for stills gallery

--- components/StillsGallery.js
+++ components/StillsGallery.js
@@ -150,44 +150,96 @@ export default function StillsGallery({ apiUrl, apiVersion, token, serviceUuid, 
         return elements.current && zoomedIdx !== null && zoomedIdx < elements.current.length - 1;
     }
 
+    const incViewPage = (direction) => {
+        setViewPage((cur) => {
+            const newPage = cur + direction;
+            if (newPage < 0 || newPage >= visiblePageCount) {
+                return cur;
+            }
+            return newPage;
+        })
+    }
+
     return (
         <>
-            <div className="flex gap-2 pb-4">
-                {[...Array(visiblePageCount).keys()].map((i) => {
-                    return (
-                        <div
-                            key={i}
-                            className={classNames(
-                                'cursor-pointer bg-gray-200 w-8 text-center border border-gray-600 rounded-sm text-gray-600',
-                                { 'text-red-500': i === viewPage }
-                            )}
-                            onClick={() => setViewPage(i)}
-                        >
-                            {i + 1}
-                        </div>
-                    );
-                })}
-            </div>
+            <nav className='relative z-0 inline-flex rounded-md shadow-sm -space-x-px' aria-label='Pagination'>
+                <button
+                    className={classNames(
+                        'relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium',
+                        {
+                            'hover:bg-gray-50 cursor-pointer text-gray-500': viewPage > 0,
+                            'cursor-default text-gray-200': viewPage === 0,
+                        }
+                    )}
+                    onClick={() => incViewPage(-1)}
+                >
+                    <span className='sr-only'>Previous</span>
+                    <svg
+                        className='h-5 w-5'
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 20 20'
+                        fill='currentColor'
+                        aria-hidden='true'
+                    >
+                        <path
+                            fillRule='evenodd'
+                            d='M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z'
+                            clipRule='evenodd'
+                        />
+                    </svg>
+                </button>
+                <div className='relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700'>
+                    <span className='font-semibold w-6 text-right'>{viewPage + 1}</span>
+                    <span className='mx-1'>of</span>
+                    <span className='font-semibold w-6 text-left'>{visiblePageCount}</span>
+                </div>
 
-            <div className={classNames("flex gap-1 sm:gap-4 flex-wrap overflow-y-auto", galleryClasses)}>
+                <button
+                    className={classNames(
+                        'relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium',
+                        {
+                            'hover:bg-gray-50 cursor-pointer text-gray-500': viewPage + 1 < visiblePageCount,
+                            'cursor-default text-gray-200': viewPage + 1 >= visiblePageCount,
+                        }
+                    )}
+                    onClick={() => incViewPage(1)}
+                >
+                    <span className='sr-only'>Next</span>
+                    <svg
+                        className='h-5 w-5'
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 20 20'
+                        fill='currentColor'
+                        aria-hidden='true'
+                    >
+                        <path
+                            fillRule='evenodd'
+                            d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
+                            clipRule='evenodd'
+                        />
+                    </svg>
+                </button>
+            </nav>
+
+            <div className={classNames('flex gap-1 mt-4 sm:gap-4 flex-wrap overflow-y-auto', galleryClasses)}>
                 {visible.map((still, i) => {
                     return (
                         <img
                             key={still.uuid}
-                            className="sm:rounded cursor-pointer"
+                            className='sm:rounded cursor-pointer'
                             src={still.data.properties.url}
                             alt={still.data.extents.temporal.min}
-                            width="300"
-                            height="200"
-                            onClick={() => setZoomedIdx(i + (viewPage * perPage))}
+                            width='300'
+                            height='200'
+                            onClick={() => setZoomedIdx(i + viewPage * perPage)}
                         />
                     );
                 })}
             </div>
 
             {zoomed && (
-                <div className="fixed z-10 inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center overflow-hidden transition">
-                    <div className="bg-white sm:rounded-lg shadow-xl sm:max-w-75w flex flex-row items-stretch">
+                <div className='fixed z-10 inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center overflow-hidden transition'>
+                    <div className='bg-white sm:rounded-lg shadow-xl sm:max-w-75w flex flex-row items-stretch'>
                         <div
                             className={classNames('w-16 flex items-center justify-center', {
                                 'text-gray-600 hover:text-gray-700 hover:bg-gray-400 cursor-pointer hover:rounded-tl-lg hover:rounded-bl-lg':
@@ -197,19 +249,19 @@ export default function StillsGallery({ apiUrl, apiVersion, token, serviceUuid, 
                             onClick={() => incZoomIdx(-1)}
                         >
                             <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-6 w-6"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
+                                xmlns='http://www.w3.org/2000/svg'
+                                className='h-6 w-6'
+                                fill='none'
+                                viewBox='0 0 24 24'
+                                stroke='currentColor'
                                 strokeWidth={2}
                             >
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M11 17l-5-5m0 0l5-5m-5 5h12" />
+                                <path strokeLinecap='round' strokeLinejoin='round' d='M11 17l-5-5m0 0l5-5m-5 5h12' />
                             </svg>
                         </div>
                         <div>
                             <img
-                                className="sm:py-8 object-contain"
+                                className='sm:py-8 object-contain'
                                 src={zoomed.data.properties.url}
                                 alt={zoomed.data.extents.temporal.min}
                                 onClick={() => setZoomedIdx(null)}
@@ -223,14 +275,14 @@ export default function StillsGallery({ apiUrl, apiVersion, token, serviceUuid, 
                             onClick={() => incZoomIdx(1)}
                         >
                             <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-6 w-6"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
+                                xmlns='http://www.w3.org/2000/svg'
+                                className='h-6 w-6'
+                                fill='none'
+                                viewBox='0 0 24 24'
+                                stroke='currentColor'
                                 strokeWidth={2}
                             >
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+                                <path strokeLinecap='round' strokeLinejoin='round' d='M13 7l5 5m0 0l-5 5m5-5H6' />
                             </svg>
                         </div>
                     </div>
