e20b5930e6256f1fc7c92738c920e73fbdf8a6c3
Dave Foster <dave@axiomdatascience.com>
2022-06-27 15:45:09 -0400

    Change cameras page to show camera summaries, not map
    WEB-59

--- pages/cameras/index.js
+++ pages/cameras/index.js
@@ -4,23 +4,20 @@ import { getSiteMetadata, getYaml } from '../../utils';
 
 import dynamic from 'next/dynamic';
 
-const WebCOOSMap = dynamic(
-    () => import('../../components/WebCOOSMap'),
-    { ssr: false }
-);
+const CameraLandingSection = dynamic(() => import('../../components/CameraLandingSection'), { ssr: false });
 
 export default function Cameras({ cameras, metadata }) {
     return (
         <Page metadata={metadata} title="Cameras">
             <Section>
                 <SectionHeader>Cameras</SectionHeader>
-                <WebCOOSMap
-                    longitude={-75.8139}
-                    latitude={36.3388}
+                <CameraLandingSection
                     mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
                     apiUrl='https://app.stage.webcoos.org/webcoos/api'
-                    token='219ec2bd77f37ce8bbb12e4c08a7b62bd506a7d3'
-                    stationSlugs={cameras.cameras.active}
+                    token={process.env.NEXT_PUBLIC_WEBCOOS_API_TOKEN}
+                    apiVersion="v1"
+                    source="webcoos"
+                    stations={cameras.cameras.active}
                 />
             </Section>
         </Page>
