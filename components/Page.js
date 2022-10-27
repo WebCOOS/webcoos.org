ace2bdcd7dcd2f0404ef942985f4513f63d6cc39
Dave Foster <dave@axiomdatascience.com>
2022-10-27 11:56:19 -0400

    Add google analytics for frontend site
    WEB-99

--- components/Page.js
+++ components/Page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Body, Footer, Header, Page as LPage } from '@axds/landing-page-components';
 import Head from 'next/head';
+import Script from 'next/script';
 
 // Most of this could move to pages/_app.js if it could fetch its own
 // static props. In the meantime we need the metadata passed in from
@@ -17,16 +18,33 @@ function Page({ children, metadata, title }) {
                     {metadata.site.title}
                 </title>
                 <meta httpEquiv='x-ua-compatible' content='ie=edge' />
-                <link
-                    href='https://api.tiles.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.css'
-                    rel='stylesheet'
-                />
+                <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.css' rel='stylesheet' />
                 {metadata.site.description && <meta name='description' content={metadata.site.description} />}
             </Head>
 
             <Header {...metadata.header} />
 
-            <Body>{children}</Body>
+            <Body>
+                {!!process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID && (
+                    <>
+                        <Script
+                            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}`}
+                            strategy='afterInteractive'
+                        />
+                        <Script id='google-analytics' strategy='afterInteractive'>
+                            {`
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){window.dataLayer.push(arguments);}
+                            gtag('js', new Date());
+
+                            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}');
+                            `}
+                        </Script>
+                    </>
+                )}
+
+                {children}
+            </Body>
 
             <Footer {...metadata.footer} />
         </LPage>
